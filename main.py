001894d6fb10bf7535f4770fd1cdc740375813e4
Minh Tran <tnnguyenminhdn@gmail.com>
2021-03-27 15:34:48 -0500

    add timred time counter, set up map API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import _thread
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from imutils import face_utils
+from datetime import datetime, timedelta
 import numpy as np
 import cv2
 import dlib
@@ -12,47 +13,55 @@ from imageio import imread
 import io
 import base64
 
+
 class Serv(BaseHTTPRequestHandler):
-	def do_GET(self):
-		if self.path == '/':
-			self.path = "/index.html"
-		try:
-			file_to_open = open(self.path[1:]).read()
-			self.send_response(200)
-		except:
-			file_to_open = "File not found"
-			self.send_response(404)
-		self.end_headers()
-		self.wfile.write(bytes(file_to_open, "utf-8"))
+    def do_GET(self):
+        if self.path == '/':
+            self.path = "/index.html"
+        try:
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_open = "File not found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_open, "utf-8"))
+
 
 def serve_server():
-	httpd = HTTPServer(("localhost", 8080), Serv)
-	httpd.serve_forever()
+    httpd = HTTPServer(("localhost", 8080), Serv)
+    httpd.serve_forever()
+
 
 def mag(vec):
-	return np.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+    return np.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+
 
 def sub(vec1, vec2):
-	return (vec1[0] - vec2[0], vec1[1] - vec2[1])
+    return (vec1[0] - vec2[0], vec1[1] - vec2[1])
+
 
 def rect_to_bb(rect):
-	x = rect.left()
-	y = rect.top()
-	w = rect.right() - x
-	h = rect.bottom() - y
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
 
-	return (x, y, w, h)
+    return (x, y, w, h)
+
 
 def shape_to_np(shape, dtype="int"):
-	coords = np.zeros((68, 2), dtype=dtype)
+    coords = np.zeros((68, 2), dtype=dtype)
 
-	for i in range(0, 68):
-		coords[i] = (shape.part(i).x, shape.part(i).y)
+    for i in range(0, 68):
+        coords[i] = (shape.part(i).x, shape.part(i).y)
 
-	return coords
+    return coords
+
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
+ap.add_argument("-p", "--shape-predictor", required=True,
+                help="path to facial landmark predictor")
 
 args = vars(ap.parse_args())
 
@@ -61,37 +70,48 @@ _thread.start_new_thread(serve_server, ())
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 
+timestamp = datetime.now()
+
+
 async def handle_socket(websocket, path):
-	while True:
-		data = await websocket.recv()
-		padding = len(data) % 4;
-		data += '=' * (4 - padding)
-		file_bytes = np.frombuffer(base64.b64decode(data), dtype=np.uint8)
-		frame = cv2.imdecode(file_bytes, flags=cv2.IMREAD_UNCHANGED)
+    while True:
+        data = await websocket.recv()
+        padding = len(data) % 4
+        data += '=' * (4 - padding)
+        file_bytes = np.frombuffer(base64.b64decode(data), dtype=np.uint8)
+        frame = cv2.imdecode(file_bytes, flags=cv2.IMREAD_UNCHANGED)
 
-		frame = imutils.resize(frame, width=500)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		rects = detector(gray, 1)
+        rects = detector(gray, 1)
 
-		for (i, rect) in enumerate(rects):
-			shape = predictor(gray, rect)
-			shape = face_utils.shape_to_np(shape)
+        for (i, rect) in enumerate(rects):
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
-			print(shape[36], shape[37], shape[38], shape[39], shape[40], shape[41])
-			print(shape[42], shape[43], shape[44], shape[45], shape[46], shape[47])
+            print(shape[36], shape[37], shape[38],
+                  shape[39], shape[40], shape[41])
+            print(shape[42], shape[43], shape[44],
+                  shape[45], shape[46], shape[47])
 
-			eye1 = (mag(sub(shape[37], shape[41])) + mag(sub(shape[38], shape[40]))) / (2 * mag(sub(shape[36], shape[39])))
-			eye2 = (mag(sub(shape[43], shape[47])) + mag(sub(shape[44], shape[46]))) / (2 * mag(sub(shape[42], shape[45])))
+            eye1 = (mag(sub(shape[37], shape[41])) + mag(sub(shape[38],
+                    shape[40]))) / (2 * mag(sub(shape[36], shape[39])))
+            eye2 = (mag(sub(shape[43], shape[47])) + mag(sub(shape[44],
+                    shape[46]))) / (2 * mag(sub(shape[42], shape[45])))
 
-			avg = (eye1 + eye2)/2
+            avg = (eye1 + eye2)/2
 
-			print(avg)
+            print(avg)
 
-			if avg < 0.18:
-				await websocket.send("::closed")
-			else:
-				await websocket.send("::open")
+            if avg < 0.18:
+                if (datetime.now() - timestamp > timedelta(seconds=2)):
+                    await websocket.send("::warning")
+                else:
+                    await websocket.send("::closed")
+            else:
+                timestamp = datetime.now()
+                await websocket.send("::open")
 
 start_server = websockets.serve(handle_socket, "localhost", 8081)
 
